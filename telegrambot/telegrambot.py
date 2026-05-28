bd37eec0525e0c3daeedc7b45cfcd16332cf9d44
BrianArandaa <brian.aranda.e@gmail.com>
2026-05-28 15:10:13 -0300

    cambio de gif y mensaje

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -27,11 +27,11 @@ async def kill(update, context):
     logging.info(update)
     logging.info(context.args)
     if context.args and context.args[0] == '@e':
-        await context.bot.send_animation(update.message.chat.id, "CgACAgEAAxkBAAICI2oYKdAqh4YkBCLifiVJZlRXy74-AAKUBwACZ_PBRLgV_qZf-9kGOwQ")
+        await context.bot.send_animation(update.message.chat.id, "CgACAgQAAxkBAAMJahiDOvbBMZVUYF4F1dYkgmKzXToAAg0DAAKBxQ1TckSOTVgP--g7BA")
         await asyncio.sleep(6)
-        await context.bot.send_message(update.message.chat.id, text="¡¡¡Ahora están todos muertos!!!")
+        await context.bot.send_message(update.message.chat.id, text="damn it")
     else:
-        await context.bot.send_message(update.message.chat.id, text="☠️ ¡¡¡Esto es muy peligroso!!! ☠️")
+        await context.bot.send_message(update.message.chat.id, text="☠️☠️☠️☠️☠️")
         
 def main():
     application = Application.builder().token(token).build()
