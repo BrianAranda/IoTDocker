8b82bed0d643c26f7416c0e8cf5dc4c02304c30c
BrianArandaa <brian.aranda.e@gmail.com>
2026-06-02 16:11:26 -0300

    Clase martes

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -3,6 +3,8 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 import logging, os, asyncio, aiomysql, traceback, locale
 import matplotlib.pyplot as plt
 from io import BytesIO
+import aiomqtt
+import ssl
 
 token=os.environ["TB_TOKEN"]
 
@@ -22,7 +24,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     kb = [["temperatura"],["humedad"],["gráfico temperatura"],["gráfico humedad"]]
     await context.bot.send_message(update.message.chat.id, text="Bienvenido al Bot "+ nombre + " " + apellido,reply_markup=ReplyKeyboardMarkup(kb))
 
-async def acercade(update: Update, context):
+async def about(update: Update, context):
     await context.bot.send_message(update.message.chat.id, text="Este bot fue creado para el curso de IoT FIO")
 
 async def kill(update: Update, context):
@@ -91,11 +93,111 @@ async def graficos(update: Update, context):
         buffer.close()
     conn.close()
 
+async def enviar_orden_mqtt(comando, valor):
+    """
+    Se conecta a Mosquitto y publica una orden para la Raspberry.
+    Recibe el 'comando' (ej. setpoint, rele) y el 'valor' (ej. 25, on).
+    """
+    
+    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    tls_context.verify_mode = ssl.CERT_REQUIRED
+    tls_context.check_hostname = True
+    tls_context.load_default_certs()
+
+    servidor = os.environ["SERVIDOR"]
+    usuario = os.environ["MQTT_USR"]
+    password = os.environ["MQTT_PASS"]
+    puerto = os.environ["PUERTO_MQTTS"]
+    
+    topico_ordenes = os.environ.get("TOPICO_PUB", "iot/termostato/comandos")
+
+    try:
+        async with aiomqtt.Client(
+            hostname=servidor,
+            port=puerto,
+            username=usuario,
+            password=password,
+            tls_context=tls_context
+        ) as client:
+            payload = f"{comando}:{valor}"
+            await client.publish(topico_ordenes, payload=payload)
+            logging.info(f"Orden MQTT enviada con éxito: {payload}")
+            return True
+
+    except Exception as e:
+        logging.error(f"Error al enviar orden MQTT: {e}")
+        return False
+
+async def comando_setpoint(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.args:
+        valor = context.args[0]
+        exito = await enviar_orden_mqtt("setpoint", valor)
+        if exito:
+            await update.message.reply_text(f"Orden enviada: Setpoint actualizado a {valor}°C")
+        else:
+            await update.message.reply_text("Error al intentar enviar la orden")
+    else:
+        await update.message.reply_text("Uso correcto: /setpoint <valor>")
+
+async def comando_rele(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.args:
+        valor = context.args[0].lower()
+        if valor in ['on', 'off']:
+            exito = await enviar_orden_mqtt("rele", valor)
+            if exito:
+                await update.message.reply_text(f"Orden enviada: Relé en modo {valor.upper()}")
+            else:
+                await update.message.reply_text("Error al intentar enviar la orden.")
+        else:
+             await update.message.reply_text("El valor del relé debe ser 'on' u 'off'.")
+    else:
+        await update.message.reply_text("Uso correcto: /rele <on/off>")
+
+async def comando_modo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.args:
+        valor = context.args[0]
+        exito = await enviar_orden_mqtt("modo", valor)
+        if exito:
+            await update.message.reply_text(f"Orden enviada: Modo cambiado a {valor}")
+        else:
+            await update.message.reply_text("Error de red al intentar enviar la orden.")
+    else:
+        await update.message.reply_text("Uso correcto: /modo <valor>")
+
+async def comando_periodo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.args:
+        valor = context.args[0]
+        exito = await enviar_orden_mqtt("periodo", valor)
+        if exito:
+            await update.message.reply_text(f"Orden enviada: Período actualizado a {valor}")
+        else:
+            await update.message.reply_text("Error de red al intentar enviar la orden.")
+    else:
+        await update.message.reply_text("Uso correcto: /periodo <valor>")
+
+async def comando_destello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.args:
+        valor = context.args[0]
+        exito = await enviar_orden_mqtt("destello", valor)
+        if exito:
+            await update.message.reply_text(f"Orden enviada: Destello actualizado a {valor}")
+        else:
+            await update.message.reply_text("Error de red al intentar enviar la orden.")
+    else:
+        await update.message.reply_text("Uso correcto: /destello <valor>")
+
 def main():
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('acercade', acercade))
+    application.add_handler(CommandHandler('about', about))
     application.add_handler(CommandHandler('kill', kill))
+
+    application.add_handler(CommandHandler('setpoint', comando_setpoint))
+    application.add_handler(CommandHandler('rele', comando_rele))
+    application.add_handler(CommandHandler('modo', comando_modo))
+    application.add_handler(CommandHandler('periodo', comando_periodo))
+    application.add_handler(CommandHandler('destello', comando_destello))
+
     application.add_handler(MessageHandler(filters.Regex("^(temperatura|humedad)$"), medicion))
     application.add_handler(MessageHandler(filters.Regex("^(gráfico temperatura|gráfico humedad)$"), graficos))
     application.run_polling()
