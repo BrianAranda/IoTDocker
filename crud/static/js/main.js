127b4e3fcaabe10bfca5e13907ab546800ed6fa0
BrianArandaa <brian.aranda.e@gmail.com>
2026-06-24 19:06:21 -0300

    Resolución Flask 1

--- crud/static/js/main.js
+++ crud/static/js/main.js
@@ -1,4 +1,17 @@
 
+// Selector de tema claro/oscuro (persiste en localStorage, claro por defecto)
+function aplicarTema(tema) {
+  document.documentElement.setAttribute('data-bs-theme', tema);
+  localStorage.setItem('tema', tema);
+}
+
+const opcionesTema = document.querySelectorAll('.theme-option');
+opcionesTema.forEach((opcion) => {
+  opcion.addEventListener('click', () => {
+    aplicarTema(opcion.getAttribute('data-tema'));
+  });
+});
+
 const btnDelete= document.querySelectorAll('.btn-borrar');
 if(btnDelete) {
   const btnArray = Array.from(btnDelete);
